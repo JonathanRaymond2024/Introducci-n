35214459fbfcbe800091d4ac62852d6513524a7f
alumno <alumno@isbo.edu.uy>
2024-04-03 08:35:55 -0300

    crear un archivo

---
+++ ejercicio1.php
@@ -0,0 +1,14 @@
+<html>
+
+    <head>
+    </head>
+    <body>
+        <h1>Mi segundo archivo PHP</h1> 
+        <?php echo <<<END
+             En un mundo conectado, 
+            donde cada segundo se envia millones de correos
+          electronicos...
+          END; ?>
+    </body>
+</html>
+

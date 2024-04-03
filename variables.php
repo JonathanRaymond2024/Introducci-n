b95aa2230204d212d9e3a9d5424ccb082723543f
alumno <alumno@isbo.edu.uy>
2024-04-03 09:03:26 -0300

    Hicimos trabajo con variables

---
+++ variables.php
@@ -0,0 +1,17 @@
+<html>
+    <head></head>
+    <body>
+        <h1>ejercico de variables</h1>
+        <?php 
+        //Texto. Puede ser con comillas simples o dobles (String)
+        $nombre = 'Manolo';
+        // Enteros (Integer)
+        $edad = 31;
+        // Decimales, usando el punto en lugar de la coma (Float)
+        $altura = 1.72; 
+        // Verdad o falso (Boolean)
+        $mayorEdad = true;
+        
+        ?>
+    </body>
+</html>

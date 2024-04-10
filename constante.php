11743c35bd87abd196169c7fe30e7bda1cc4d38c
alumno <alumno@isbo.edu.uy>
2024-04-10 09:07:26 -0300

    Mas cosas dadas en clase 2

---
+++ constante.php
@@ -0,0 +1,14 @@
+<html>
+
+    <head>
+    </head>
+    <body>
+    
+
+        <h1>Trabajo con constante</h1>
+        <?php  
+        define ('GRAVEDAD', 9.8);
+        const GRAVEDAD = 9.8;
+        ?>
+    </body>
+</html>

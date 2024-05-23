1e448b7f80ec7013cdbb874b68bc8a8ff16da250
JonathanRaymond2024 <jonathanraymond84@gmail.com>
2024-05-23 17:00:46 -0300

    Jugandos con string

---
+++ JugandoConStrings.php
@@ -0,0 +1,16 @@
+<?php  
+    $palabra = 'abcdef';
+    echo $palabra;
+    echo "<br>";
+    $palabra[2] = 'z';
+    echo $palabra ;
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    
+?>
+<?php
+$lista = "manzana,naranja,platano,fresa";
+$frutas= explode (",",$lista);
+print_r($frutas);
+?>

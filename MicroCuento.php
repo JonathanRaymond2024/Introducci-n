40b544c24e0139a094bf96f1c0e1e0bb2b7b0ddd
JonathanRaymond2024 <jonathanraymond84@gmail.com>
2024-05-23 17:08:44 -0300

    Micro cuento

---
+++ MicroCuento.php
@@ -0,0 +1,8 @@
+<?php 
+$cuento= "En el reino de las sombras, donde el rock era una leyenda olvidada, dos guerreros de la música emergieron de la oscuridad: Jack y Kyle, conocidos como Tenacious D. Armados con guitarras mágicas, desafiaron al tirano del silencio, el Señor de la Discordia. En un duelo de riffs y poder vocal, derrotaron al opresor, devolviendo la melodía al mundo. Su música resonó como un trueno, inspirando a las masas a rebelarse contra el silencio impuesto. Así, la leyenda de Tenacious D se grabó en la historia como los salvadores del rock, los heraldos del renacimiento musical.";
+print $cuento;
+print "<br>";
+$palabras = explode (" ", $cuento);
+ print_r($palabras);
+
+?>
